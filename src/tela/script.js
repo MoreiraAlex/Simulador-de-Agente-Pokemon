1c779349cebb2a0dff51c498090c7ab09d5b9b0c
mooreiralex <alexmoreira.dev@gmail.com>
2025-04-04 20:42:48 -0300

    ajustando cronomoetro

--- src/tela/script.js
+++ src/tela/script.js
@@ -1,92 +1,94 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.querySelector('#game-canvas')
-  const ctx = canvas.getContext('2d')
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.querySelector("#game-canvas");
+  const ctx = canvas.getContext("2d");
 
-  referencia(canvas, ctx)
-  controlaMapa(canvas)
-  adicionaRemoveTreinador()
-  simulação()
-})
+  const config = {
+    contadorTreinadores: 0,
+  };
+
+  referencia(canvas, ctx);
+  controlaMapa(canvas);
+  adicionaRemoveTreinador(config);
+  GUI(config);
+});
 
 function referencia(canvas, ctx) {
-  const centerX = canvas.width / 2
-  const centerY = canvas.height / 2
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
 
   // Quadrado vermelho
-  ctx.fillStyle = 'red'
-  ctx.fillRect(centerX - 10, centerY - 10, 20, 20)
+  ctx.fillStyle = "red";
+  ctx.fillRect(centerX - 10, centerY - 10, 20, 20);
 
   // Círculo azul
-  ctx.fillStyle = 'blue'
-  ctx.beginPath()
-  ctx.arc(centerX, centerY, 5, 0, Math.PI * 2)
-  ctx.fill()
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, 5, 0, Math.PI * 2);
+  ctx.fill();
 
   // Linhas cruzadas (Eixo X e Y)
-  ctx.strokeStyle = 'black'
-  ctx.lineWidth = 2
-  ctx.beginPath()
-  ctx.moveTo(centerX - 100, centerY)
-  ctx.lineTo(centerX + 100, centerY)
-  ctx.moveTo(centerX, centerY - 100)
-  ctx.lineTo(centerX, centerY + 100)
-  ctx.stroke()
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(centerX - 100, centerY);
+  ctx.lineTo(centerX + 100, centerY);
+  ctx.moveTo(centerX, centerY - 100);
+  ctx.lineTo(centerX, centerY + 100);
+  ctx.stroke();
 }
 
 function controlaMapa(canvas) {
-  const container = canvas.parentElement
-  let isDragging = false
-  let startX, startY, scrollLeft, scrollTop
+  const container = canvas.parentElement;
+  let isDragging = false;
+  let startX, startY, scrollLeft, scrollTop;
 
-  canvas.addEventListener('mousedown', (e) => {
-    isDragging = true
-    startX = e.pageX - canvas.offsetLeft
-    startY = e.pageY - canvas.offsetTop
-    scrollLeft = container.scrollLeft
-    scrollTop = container.scrollTop
-    canvas.style.cursor = 'grabbing'
-  })
+  canvas.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - canvas.offsetLeft;
+    startY = e.pageY - canvas.offsetTop;
+    scrollLeft = container.scrollLeft;
+    scrollTop = container.scrollTop;
+    canvas.style.cursor = "grabbing";
+  });
 
-  canvas.addEventListener('mouseleave', () => {
-    isDragging = false
-    canvas.style.cursor = 'grab'
-  })
+  canvas.addEventListener("mouseleave", () => {
+    isDragging = false;
+    canvas.style.cursor = "grab";
+  });
 
-  canvas.addEventListener('mouseup', () => {
-    isDragging = false
-    canvas.style.cursor = 'grab'
-  })
+  canvas.addEventListener("mouseup", () => {
+    isDragging = false;
+    canvas.style.cursor = "grab";
+  });
 
-  canvas.addEventListener('mousemove', (e) => {
-    if (!isDragging) return
-    e.preventDefault()
-    const x = e.pageX - canvas.offsetLeft
-    const y = e.pageY - canvas.offsetTop
-    const walkX = (x - startX) * -1
-    const walkY = (y - startY) * -1
-    container.scrollLeft = scrollLeft + walkX
-    container.scrollTop = scrollTop + walkY
-  })
+  canvas.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - canvas.offsetLeft;
+    const y = e.pageY - canvas.offsetTop;
+    const walkX = (x - startX) * -1;
+    const walkY = (y - startY) * -1;
+    container.scrollLeft = scrollLeft + walkX;
+    container.scrollTop = scrollTop + walkY;
+  });
 }
 
-function adicionaRemoveTreinador() {
-  const btnAdicionar = document.querySelector('.adicionar')
-  const listaTreinadores = document.querySelector('.treinadores-lista')
+function adicionaRemoveTreinador(config) {
+  const btnAdicionar = document.querySelector(".adicionar");
+  const listaTreinadores = document.querySelector(".treinadores-lista");
+  const maxTreinadores = 4;
 
-  let contadorTreinadores = 0
-  const maxTreinadores = 5
-
-  btnAdicionar.addEventListener('click', () => {
-    if (contadorTreinadores >= maxTreinadores) {
+  btnAdicionar.addEventListener("click", () => {
+    if (config.contadorTreinadores >= maxTreinadores) {
       // eslint-disable-next-line no-undef
-      alert('Limite máximo de 5 treinadores atingido!')
-      return
+      alert(`Limite máximo de ${maxTreinadores} treinadores atingido!`);
+      return;
     }
 
-    contadorTreinadores++
+    config.contadorTreinadores++;
 
-    const treinador = document.createElement('div')
-    treinador.id = `${contadorTreinadores}`
+    const treinador = document.createElement("div");
+    treinador.id = `${config.contadorTreinadores}`;
 
     treinador.innerHTML = `
         <div class="bg-gray-800 p-4 rounded-lg shadow-md border border-gray-600">
@@ -102,154 +104,138 @@ function adicionaRemoveTreinador() {
                 <div class="checkbox-group">
                     <p class="font-bold">Estratégia:</p>
                     <label class="block">
-                        <input type="radio" name="estrategia-${contadorTreinadores}" value="agressivo" checked class="config atributo form-radio text-blue-500"> Agressivo
+                        <input type="radio" name="estrategia-${config.contadorTreinadores}" value="agressivo" checked class="config atributo form-radio text-blue-500"> Agressivo
                     </label>
                     <label class="block">
-                        <input type="radio" name="estrategia-${contadorTreinadores}" value="cauteloso" class="config atributo form-radio text-blue-500"> Cauteloso
+                        <input type="radio" name="estrategia-${config.contadorTreinadores}" value="cauteloso" class="config atributo form-radio text-blue-500"> Cauteloso
                     </label>
                 </div>
 
                 <div class="checkbox-group">
                     <p class="font-bold">Pokémon:</p>
                     <label class="block">
-                        <input type="radio" name="pokemonInicial-${contadorTreinadores}" value="bulbasaur" checked class="config atributo form-radio text-blue-500"> Bulbasaur
+                        <input type="radio" name="pokemonInicial-${config.contadorTreinadores}" value="bulbasaur" checked class="config atributo form-radio text-blue-500"> Bulbasaur
                     </label>
                     <label class="block">
-                        <input type="radio" name="pokemonInicial-${contadorTreinadores}" value="charmander" class="config atributo form-radio text-blue-500"> Charmander
+                        <input type="radio" name="pokemonInicial-${config.contadorTreinadores}" value="charmander" class="config atributo form-radio text-blue-500"> Charmander
                     </label>
                     <label class="block">
-                        <input type="radio" name="pokemonInicial-${contadorTreinadores}" value="squirtle" class="config atributo form-radio text-blue-500"> Squirtle
+                        <input type="radio" name="pokemonInicial-${config.contadorTreinadores}" value="squirtle" class="config atributo form-radio text-blue-500"> Squirtle
                     </label>
                 </div>
             </div>
         </div>
-      `
+      `;
 
-    listaTreinadores.appendChild(treinador)
+    listaTreinadores.appendChild(treinador);
 
-    treinador.querySelector('.remover').addEventListener('click', () => {
-      treinador.remove()
-      contadorTreinadores--
-    })
-  })
+    treinador.querySelector(".remover").addEventListener("click", () => {
+      treinador.remove();
+      config.contadorTreinadores--;
+    });
+  });
 }
 
-function simulação() {
-  const btnIniciar = document.querySelector('.iniciar')
-  const btnPausar = document.querySelector('.pausar')
-  const btnParar = document.querySelector('.parar')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function GUI(config) {
+  const btnIniciar = document.querySelector(".iniciar");
+  const btnPausar = document.querySelector(".pausar");
+  const btnParar = document.querySelector(".parar");
 
-  const relogio = document.getElementById('relogio')
+  const relogio = document.getElementById("relogio");
+  const listaTreinadores = document.querySelector(".treinadores-lista");
 
-  const listaTreinadores = document.querySelector('.treinadores-lista')
+  const treinadores = [];
 
-  const treinadores = []
+  const tempo = {
+    interval: null,
+    conteudo: relogio,
+    segundos: 0,
+  };
 
-  let interval = null
-  const segundos = 0
-
-  btnIniciar.addEventListener('click', () => {
+  btnIniciar.addEventListener("click", () => {
     if (listaTreinadores.children.length === 0) {
       // eslint-disable-next-line no-undef
-      alert('Adicione pelo menos um treinador antes de iniciar a simulação!')
-      return
+      alert("Adicione pelo menos um treinador antes de iniciar a simulação!");
+      return;
     }
 
-    Array.from(listaTreinadores.children).forEach((t) => {
-      const treinador = {
-        id: t.id,
-        pokemons: [],
-        equipe: [],
-      }
+    simulação(tempo, listaTreinadores, treinadores);
 
-      t.querySelectorAll('.atributo').forEach((elemento) => {
-        if (elemento.type === 'radio') {
-          if (elemento.checked)
-            treinador[elemento.name.split('-')[0]] = elemento.value
-        } else {
-          treinador[elemento.name] = elemento.value
-        }
-      })
+    btnIniciar.textContent = "Simulando...";
+    btnIniciar.disabled = true;
+    btnIniciar.style.opacity = "0.5";
 
-      treinador.pokemons.push(treinador.pokemonInicial)
-      treinador.equipe.push(treinador.pokemonInicial)
+    btnPausar.disabled = false;
+    btnPausar.style.opacity = "1";
 
-      treinadores.push(treinador)
-    })
+    btnParar.disabled = false;
+    btnParar.style.opacity = "1";
 
-    interval = contador(relogio, segundos)
-    document.querySelectorAll('.config').forEach((elemento) => {
+    document.querySelectorAll(".config").forEach((elemento) => {
       if (!elemento.checked) {
-        elemento.disabled = true
-        elemento.style.opacity = '0.5'
+        elemento.disabled = true;
+        elemento.style.opacity = "0.5";
       }
-    })
+    });
 
-    btnIniciar.textContent = 'Simulando...'
-    btnIniciar.disabled = true
-    btnIniciar.style.opacity = '0.5'
+    DetalhesTreinador(treinadores);
+  });
 
-    btnPausar.disabled = false
-    btnPausar.style.opacity = '1'
-
-    btnParar.disabled = false
-    btnParar.style.opacity = '1'
-
-    DetalhesTreinador(treinadores)
-  })
-
-  btnPausar.addEventListener('click', () => {
-    if (btnPausar.textContent === 'Pausar') {
-      btnPausar.textContent = 'Continuar'
-      clearInterval(interval)
+  btnPausar.addEventListener("click", () => {
+    if (btnPausar.textContent === "Pausar") {
+      btnPausar.textContent = "Continuar";
+      clearInterval(tempo.interval);
     } else {
-      btnPausar.textContent = 'Pausar'
-      interval = contador(relogio, segundos)
+      btnPausar.textContent = "Pausar";
+      simulação(tempo);
     }
-  })
+  });
 
-  btnParar.addEventListener('click', () => {
-    clearInterval(interval)
+  btnParar.addEventListener("click", () => {
+    clearInterval(tempo.interval);
+    tempo.conteudo.textContent = "00:00";
+    tempo.segundos = 0;
 
-    relogio.textContent = '00:00'
+    config.contadorTreinadores = 0;
 
-    btnIniciar.textContent = 'Iniciar'
-    btnIniciar.disabled = false
-    btnIniciar.style.opacity = '1'
+    btnIniciar.textContent = "Iniciar";
+    btnIniciar.disabled = false;
+    btnIniciar.style.opacity = "1";
 
-    btnPausar.disabled = true
-    btnPausar.style.opacity = '0.5'
+    btnPausar.disabled = true;
+    btnPausar.style.opacity = "0.5";
 
-    btnParar.disabled = true
-    btnParar.style.opacity = '0.5'
+    btnParar.disabled = true;
+    btnParar.style.opacity = "0.5";
 
-    document.querySelectorAll('.config').forEach((elemento) => {
-      elemento.disabled = false
-      elemento.style.opacity = '1'
-    })
+    document.querySelectorAll(".config").forEach((elemento) => {
+      elemento.disabled = false;
+      elemento.style.opacity = "1";
+    });
 
-    treinadores.length = 0
+    treinadores.length = 0;
     while (listaTreinadores.firstChild) {
-      listaTreinadores.removeChild(listaTreinadores.firstChild)
+      listaTreinadores.removeChild(listaTreinadores.firstChild);
     }
 
-    const footer = document.querySelector('footer')
+    const footer = document.querySelector("footer");
     while (footer.firstChild) {
-      footer.removeChild(footer.firstChild)
+      footer.removeChild(footer.firstChild);
     }
-  })
+  });
 }
 
 function DetalhesTreinador(treinadores) {
-  const footer = document.querySelector('footer')
+  const footer = document.querySelector("footer");
   treinadores.forEach((treinador) => {
-    const container = document.createElement('div')
+    const container = document.createElement("div");
     container.className =
-      'flex justify-between items-center bg-gray-900 p-2 rounded-md border border-gray-600'
+      "flex justify-between items-center bg-gray-900 p-2 rounded-md border border-gray-600";
 
     // Lado esquerdo: atributos
-    const atributosDiv = document.createElement('div')
-    atributosDiv.className = 'text-sm'
+    const atributosDiv = document.createElement("div");
+    atributosDiv.className = "text-sm";
     atributosDiv.innerHTML = `
       <p><strong>Treinador ${treinador.id}</strong></p>
       <p>Velocidade: ${treinador.velocidade}</p>
@@ -257,47 +243,72 @@ function DetalhesTreinador(treinadores) {
       <p>Campo de Visão: ${treinador.visao}</p>
       <p>Estratégia: ${treinador.estrategia}</p>
       <p>Pokemons Capturados: ${treinador.pokemons.length}</p>
-  `
+  `;
 
     // Lado direito: pokemons
-    const equipeDiv = document.createElement('div')
-    equipeDiv.className = 'flex space-x-2'
+    const equipeDiv = document.createElement("div");
+    equipeDiv.className = "flex space-x-2";
 
     for (let i = 0; i < 4; i++) {
-      const slot = document.createElement('div')
+      const slot = document.createElement("div");
       slot.className =
-        'w-10 h-10 bg-gray-700 border border-gray-500 rounded flex items-center justify-center text-xs text-center'
-      slot.textContent = treinador.equipe[i] || '—'
-      equipeDiv.appendChild(slot)
+        "w-10 h-10 bg-gray-700 border border-gray-500 rounded flex items-center justify-center text-xs text-center";
+      slot.textContent = treinador.equipe[i] || "—";
+      equipeDiv.appendChild(slot);
     }
-    container.appendChild(atributosDiv)
-    container.appendChild(equipeDiv)
-    footer.appendChild(container)
-  })
+    container.appendChild(atributosDiv);
+    container.appendChild(equipeDiv);
+    footer.appendChild(container);
+  });
 }
 
-function contador(relogio, segundos) {
-  return setInterval(() => {
-    segundos++
-    const minutos = Math.floor(segundos / 60)
-    const seg = segundos % 60
-    relogio.textContent = `${String(minutos).padStart(2, '0')}:${String(seg).padStart(2, '0')}`
-  }, 1000)
+function simulação(tempo, listaTreinadores, treinadores) {
+  Array.from(listaTreinadores.children).forEach((t) => {
+    const treinador = {
+      id: t.id,
+      pokemons: [],
+      equipe: [],
+    };
+
+    t.querySelectorAll(".atributo").forEach((elemento) => {
+      if (elemento.type === "radio") {
+        if (elemento.checked)
+          treinador[elemento.name.split("-")[0]] = elemento.value;
+      } else {
+        treinador[elemento.name] = elemento.value;
+      }
+    });
+
+    treinador.pokemons.push(treinador.pokemonInicial);
+    treinador.equipe.push(treinador.pokemonInicial);
+
+    treinadores.push(treinador);
+  });
+
+  tempo.interval = setInterval(() => {
+    tempo.segundos++;
+    const minutos = Math.floor(tempo.segundos / 60);
+    const seg = tempo.segundos % 60;
+    tempo.conteudo.textContent = `${String(minutos).padStart(2, "0")}:${String(seg).padStart(2, "0")}`;
+  }, 1000);
+
+  DetalhesTreinador(treinadores);
 }
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function toggleInput() {
-  const tempoInput = document.getElementById('input-tempo')
-  const capturaInput = document.getElementById('input-captura')
+  const tempoInput = document.getElementById("input-tempo");
+  const capturaInput = document.getElementById("input-captura");
   const selectedValue = document.querySelector(
     'input[name="vitoria"]:checked',
-  ).value
+  ).value;
 
-  tempoInput.style.display = 'none'
-  capturaInput.style.display = 'none'
+  tempoInput.style.display = "none";
+  capturaInput.style.display = "none";
 
-  if (selectedValue === '0') {
-    tempoInput.style.display = 'block'
-  } else if (selectedValue === '1') {
-    capturaInput.style.display = 'block'
+  if (selectedValue === "0") {
+    tempoInput.style.display = "block";
+  } else if (selectedValue === "1") {
+    capturaInput.style.display = "block";
   }
 }
