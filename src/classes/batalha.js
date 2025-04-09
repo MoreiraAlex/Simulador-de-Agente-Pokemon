a78a0c987fd662755ecfcfa93a2ab5f4b3f8aefd
Alex <sander.alex.nascimento@gmail.com>
2025-04-09 15:26:55 -0300

    Finalizando a primeira versão da classe batalha

--- src/classes/batalha.js
+++ src/classes/batalha.js
@@ -260,8 +260,12 @@ class Batalha {
     };
     this.treinador = treinador;
     this.batalhaAtiva = true;
+    this.lutaAtiva = false;
+    this.pokemonsAtacante = [];
+    this.pokemonsDefensor = [];
     this.intervalo1 = null;
     this.intervalo2 = null;
+    this.vencedor = null;
   }
 
   // sinceramente n sei pq ta dando erro aq, mas essa função que determina se existe vantagem ou desvantagem
@@ -371,26 +375,34 @@ class Batalha {
 
   luta(p1, p2) {
     if (!this.intervalo1) {
+      console.log(`\n${p1.nome} vs ${p2.nome}\n`);
+
       this.intervalo1 = setInterval(() => {
         if (!this.lutaAtiva) {
           clearInterval(this.intervalo1);
+          this.intervalo1 = null;
           return;
         }
+
         const dano = this.atacar(p1, p2);
 
         p2.hp -= dano;
+
         console.log(`${p2.nome} perdeu ${dano} de vida, hp restante: ${p2.hp}`);
 
         if (p2.hp <= 0) {
-          console.log(``);
-          console.log(`${p1.nome} derrotou ${p2.nome}`);
+          console.log(`\n${p1.nome} derrotou ${p2.nome}`);
 
           this.lutaAtiva = false;
 
           clearInterval(this.intervalo1);
+          this.intervalo1 = null;
+
           if (this.intervalo2) {
             clearInterval(this.intervalo2);
+            this.intervalo2 = null;
           }
+          this.vencedor = p1;
         }
       }, 1000 / p1.ataques.atkBasico[1]);
 
@@ -401,6 +413,7 @@ class Batalha {
       this.intervalo2 = setInterval(() => {
         if (!this.lutaAtiva) {
           clearInterval(this.intervalo2);
+          this.intervalo2 = null;
           return;
         }
         const dano = this.atacar(p2, p1);
@@ -409,24 +422,129 @@ class Batalha {
         console.log(`${p1.nome} perdeu ${dano} de vida, hp restante: ${p1.hp}`);
 
         if (p1.hp <= 0) {
-          console.log(``);
-          console.log(`${p2.nome} derrotou ${p1.nome}`);
-          console.log(``);
+          console.log(`\n${p2.nome} derrotou ${p1.nome}`);
           this.lutaAtiva = false;
 
           clearInterval(this.intervalo2);
+          this.intervalo2 = null;
           if (this.intervalo1) {
             clearInterval(this.intervalo1);
+            this.intervalo1 = null;
           }
+
+          this.vencedor = p2;
         }
       }, 1000 / p2.ataques.atkBasico[1]); // Aqui é o tempo de ataque do Pokémon 2
     }
+    return this.vencedor;
   }
 
   batalha(atacante, defensor, frame) {
     const intervalo = setInterval(() => {
       if (!this.batalhaAtiva) {
         clearInterval(intervalo);
+
+        this.pokemonsAtacante = Object.values(
+          this.pokemonsAtacante.reduce((acc, item) => {
+            if (!acc[item.id]) {
+              acc[item.id] = { id: item.id, nocautes: 0 };
+            }
+            acc[item.id].nocautes += item.nocautes;
+            return acc;
+          }, {}),
+        );
+
+        this.pokemonsDefensor = Object.values(
+          this.pokemonsDefensor.reduce((acc, item) => {
+            if (!acc[item.id]) {
+              acc[item.id] = { id: item.id, nocautes: 0 };
+            }
+            acc[item.id].nocautes += item.nocautes;
+            return acc;
+          }, {}),
+        );
+
+        let resultado = {};
+        if (atacante.equipe[0].hp <= 0) {
+          resultado = {
+            vencedor: {
+              id: defensor.id,
+              pokemons: this.pokemonsDefensor,
+            },
+            perdedor: {
+              id: atacante.id,
+              pokemons: this.pokemonsAtacante,
+            },
+          };
+        } else {
+          resultado = {
+            vencedor: {
+              id: atacante.id,
+              pokemons: this.pokemonsAtacante,
+            },
+            perdedor: {
+              id: defensor.id,
+              pokemons: this.pokemonsDefensor,
+            },
+          };
+        }
+        console.log(`\nTreinador ${resultado.vencedor.id} venceu`);
+        return resultado;
+      }
+      // criar a porra do sort de lv e verificação de vivo ou morto
+
+      if (!this.lutaAtiva) {
+        atacante.equipe.sort((a, b) => b.hp - a.hp);
+        defensor.equipe.sort((a, b) => b.hp - a.hp);
+
+        // console.log(atacante.equipe);
+        // console.log(defensor.equipe);
+
+        if (atacante.equipe[0].hp <= 0 || defensor.equipe[0].hp <= 0) {
+          this.batalhaAtiva = false;
+          return;
+        }
+        this.lutaAtiva = true;
+        return;
+      }
+
+      const pokemonAtacante = atacante.equipe[0];
+      const pokemonDefensor = defensor.equipe[0];
+
+      this.luta(pokemonAtacante, pokemonDefensor);
+
+      if (this.vencedor) {
+        atacante.equipe[0] = pokemonAtacante;
+        defensor.equipe[0] = pokemonDefensor;
+        const vencedor =
+          this.vencedor.id === pokemonAtacante.id
+            ? pokemonAtacante
+            : pokemonDefensor;
+        const perdedor =
+          this.vencedor.id === pokemonAtacante.id
+            ? pokemonDefensor
+            : pokemonAtacante;
+
+        if (vencedor === pokemonAtacante) {
+          this.pokemonsAtacante.push({
+            id: vencedor.id,
+            nocaute: 1,
+          });
+
+          this.pokemonsDefensor.push({
+            id: perdedor.id,
+            nocaute: 0,
+          });
+        } else {
+          this.pokemonsDefensor.push({
+            id: vencedor.id,
+            nocaute: 1,
+          });
+          this.pokemonsAtacante.push({
+            id: perdedor.id,
+            nocaute: 0,
+          });
+        }
       }
     }, frame);
   }
@@ -444,82 +562,87 @@ class Batalha {
     return this.treinador;
   }
 }
+
 export default Batalha;
-const pokemons = [
+
+const treinadores = [
   {
-    nome: "Charmander",
-    nivel: 5,
-    xp: 0,
-    hp: 100,
-    ataque: 10,
-    defesa: 6,
-    tipos: ["fogo"],
-    ataques: {
-      atkBasico: [10, 1],
-      skill: [20, 5],
-    },
+    id: 1,
+    equipe: [
+      {
+        nome: "Pidgey",
+        nivel: 5,
+        xp: 0,
+        hp: 100,
+        ataque: 10,
+        defesa: 6,
+        tipos: ["normal", "voador"],
+        ataques: {
+          atkBasico: [10, 1],
+          skill: [20, 5],
+        },
+      },
+      {
+        nome: "Rattata",
+        nivel: 5,
+        xp: 0,
+        hp: 100,
+        ataque: 10,
+        defesa: 6,
+        tipos: ["normal"],
+        ataques: {
+          atkBasico: [10, 1],
+          skill: [20, 5],
+        },
+      },
+    ],
   },
   {
-    nome: "Squirtle",
-    nivel: 5,
-    xp: 0,
-    hp: 100,
-    ataque: 10,
-    defesa: 5,
-    tipos: ["agua"],
-    ataques: {
-      atkBasico: [10, 1.2],
-      skill: [20, 5],
-    },
-  },
-  {
-    nome: "Bulbasaur",
-    nivel: 5,
-    xp: 0,
-    hp: 100,
-    ataque: 10,
-    defesa: 123,
-    tipos: ["grama", "veneno"],
-    ataques: {
-      atkBasico: [10, 1],
-      skill: [20, 5],
-    },
+    id: 2,
+    equipe: [
+      {
+        nome: "Charmander",
+        nivel: 5,
+        xp: 0,
+        hp: 100,
+        ataque: 10,
+        defesa: 6,
+        tipos: ["fogo"],
+        ataques: {
+          atkBasico: [10, 1],
+          skill: [20, 5],
+        },
+      },
+      {
+        nome: "Squirtle",
+        nivel: 5,
+        xp: 0,
+        hp: 100,
+        ataque: 10,
+        defesa: 5,
+        tipos: ["agua"],
+        ataques: {
+          atkBasico: [10, 1.2],
+          skill: [20, 5],
+        },
+      },
+      {
+        nome: "Bulbasaur",
+        nivel: 5,
+        xp: 0,
+        hp: 100,
+        ataque: 10,
+        defesa: 6,
+        tipos: ["grama"],
+        ataques: {
+          atkBasico: [10, 1],
+          skill: [20, 5],
+        },
+      },
+    ],
   },
 ];
-const timeTreinador = [
-  {
-    nome: "Pidgey",
-    nivel: 5,
-    xp: 0,
-    hp: 100,
-    ataque: 10,
-    defesa: 6,
-    tipos: ["normal", "voador"],
-    ataques: {
-      atkBasico: [10, 1],
-      skill: [20, 5],
-    },
-  },
-  {
-    nome: "Rattata",
-    nivel: 5,
-    xp: 0,
-    hp: 100,
-    ataque: 10,
-    defesa: 6,
-    tipos: ["normal"],
-    ataques: {
-      atkBasico: [10, 1],
-      skill: [20, 5],
-    },
-  },
-];
+
 const batalha = new Batalha("Red");
 
-const intervalo = setInterval(() => {
-  batalha.luta(timeTreinador[0], pokemons[1], timeTreinador);
-  // .analisarTimeContraSelvagem(pokemons[1].tipos, timeTreinador);
-  if (batalha.batalhaAtiva === false) {
-    clearInterval(intervalo);
-  }
-});
+batalha.batalha(treinadores[0], treinadores[1], 1000);
